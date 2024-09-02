04b1bca8cf0e918dc4c7e206e4e846b86e668360
youssou diarra ndiaye <youssoudiarrandiaye52@gmail.com>
2024-09-02 16:47:57 +0000

    D1

--- gateway-service/src/main/java/net/momodev/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/net/momodev/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,11 @@ package net.momodev.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -9,5 +14,10 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+    }
+
 
 }
